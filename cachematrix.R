fbce495cdab613425847d496a896b388174f9ffb
Amanda Walter <amanda.walter@octanner.com>
2016-05-02 14:04:52 -0600

    functions completed

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,29 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This script will find the inverse of a matrix and store it in memory to be called more quickly in the future.
 
-## Write a short comment describing this function
-
+## This function will create a list of operations that can be used to store and retreive the cached matrix
 makeCacheMatrix <- function(x = matrix()) {
-
+    memInverse <- NULL
+    set <- function(y){
+        x <<- y
+        memInverse <<- NULL
+    }
+    get <- function() x
+    setmatrix <- function(I) memInverse <<- I
+    getmatrix <- function() memInverse
+    list(set = set, get = get, setmatrix = setmatrix, getmatrix = getmatrix)
 }
 
-
-## Write a short comment describing this function
+## This function will return an inverse matrix of x.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    memInverse <- x$getmatrix()
+    if(!is.null(memInverse)) {
+        message("Retreiving stored inverse matrix")
+        return(memInverse)
+    }
+    sq_matrix <- x$get()
+    memInverse <- solve(sq_matrix)
+    x$setmatrix(memInverse)
+    memInverse
 }
